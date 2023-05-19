45c34c55531b8b5960a44b108b3ca740a158165e
fnakhsan <fn.akhsan@gmail.com>
2023-05-19 21:15:47 +0700

    Add package and dummy data

---
+++ app/src/main/java/com/example/programminglanguagecompose/ui/values/Dimens.kt
@@ -0,0 +1,10 @@
+package com.example.programminglanguagecompose.ui.values
+
+import androidx.compose.ui.unit.dp
+
+val spacingTiny = 4.dp
+val spacingSmaller = 8.dp
+val spacingSmall = 12.dp
+val spacingRegular = 16.dp
+val spacingLarge = 20.dp
+val spacingLarger = 24.dp
